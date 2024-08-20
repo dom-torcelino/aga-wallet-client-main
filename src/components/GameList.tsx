af2be6d4aa3bbb9806bfa8156af5c18742e0d371
domdomdom <torcelino.johndominic@gmail.com>
2024-08-20 16:50:50 +0800

    game list theme

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -48,9 +48,9 @@ export const GameList: React.FC<GameListProps> = ({data = []}) => {
       key={item.game_id.toString()}
       onPress={() => navigation.navigate('GameView', {game: item})}>
       <Image source={{uri: item.game_image}} style={styles.ImageStyles} />
-      <Text style={[styles.TextStyles, {color: theme.textColor}]}>{item.game_name}</Text>
+      <Text style={styles.TextStyles}>{item.game_name}</Text>
       <View style={styles.infoRow}>
-        <Text style={[styles.TextStyles2, {color: theme.secondaryTextColor}]}>Total Players: {item.game_players}</Text>
+        <Text style={styles.TextStyles2}>Total Players: {item.game_players}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -63,21 +63,26 @@ export const GameList: React.FC<GameListProps> = ({data = []}) => {
         keyExtractor={item => item.game_id.toString()}
         scrollEnabled={false}
       />
+       
     </View>
+    
   );
 };
+
 
 const styles = StyleSheet.create({
   container: {
     marginBottom: 20,
   },
   itemContainer: {
-    width: itemWidth, 
+    width: itemWidth, // Set item width dynamically
     alignItems: 'flex-start',
     // marginBottom: 10,
     margin: 6,
     borderRadius: BORDERRADIUS.radius_15,
+    backgroundColor: COLORS.secondaryBGColor,
     borderWidth: 1,
+    borderColor: COLORS.borderStroke,
     overflow: 'hidden',
   },
   TextStyles: {
@@ -93,8 +98,8 @@ const styles = StyleSheet.create({
     fontSize:13,
   },
   ImageStyles: {
-    width: itemWidth, 
-    height: itemWidth * 0.4, 
+    width: itemWidth, // Make image width responsive
+    height: itemWidth * 0.4, // Adjust height based on aspect ratio (0.6 here as an example)
   },
   infoRow: {
     flexDirection: 'row',
